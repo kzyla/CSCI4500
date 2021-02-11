13b8ffeb248d10b03f86f6e84bb4c0e7ec77d94c
Kyle Zyla <kzyla@unomaha.edu>
2021-02-10 18:20:26 -0800

    updates

--- prog1.c
+++ prog1.c
@@ -2,20 +2,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
 
-void * thread_function(void *data) {
-	long seconds = *((long*)data); // cast pointer to long 
-	int i;
+double logValue = 0;
 
-	// Loop through number of seconds 		
-	for(i = 0; i < seconds; i++)
-	{
-		sleep(1);
-		printf("Thread %lu  count: %d\n", seconds, i);
-	}
-
-	printf("Thread %lu exiting...\n", seconds);
-	pthread_exit(data); // return pointer to the number of seconds pass to us
+void * log_function(void *data) {
+	pthread_exit(data);
 }
 
 int main(int argc, char *argv[]) {
@@ -24,40 +16,41 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	// const int INTEGER = atoi(argv[1])
+	const double VALUE = atof(argv[1]);
+	if (VALUE <= 0 || VALUE > 2) {
+		printf("Please provide an integer between the interval 0 < x <= 2\n");
+		return 0;
+	}
+
 	const int MAX_THREADS = atoi(argv[2]);	
 	// const int ITERATIONS = atoi(argv[3]);
 
-	// Table of thread IDs
-	pthread_t	threadID_table[MAX_THREADS]; 
-	// Table of seconds values passed to each thread
-	long		seconds[MAX_THREADS];
+	logValue = VALUE;
 
-	for(int i = 0; i < MAX_THREADS; i++) {
+	pthread_t threadID_table[MAX_THREADS]; 
+
+	for (int j = 0; j < MAX_THREADS; j++) {
 		int thread_create_status = 0;	
 
-		seconds[i] = i + 1; // Set number of seconds
-		thread_create_status = pthread_create(&threadID_table[i], NULL, 
-					thread_function, ((void*) &seconds[i]) );
+		thread_create_status = pthread_create(&threadID_table[j], NULL, log_function, NULL);
 
-		if(thread_create_status != 0) {
+		if (thread_create_status != 0) {
 			printf("pthread_create error\n");
 		}
 	}
 
-	for(int i = 0; i < MAX_THREADS; i++) {
-                int * thread_retval;
-                int   join_retval;
+	for (int i = 0; i < MAX_THREADS; i++) {
+		int * thread_retval;
+		int   join_retval;
 		
 		join_retval = pthread_join(threadID_table[i], (void**) (&thread_retval) );
-		if(join_retval == 0) {
-			printf("join> thread retval: %d join retval: %d\n", 
-				*thread_retval, join_retval);
-		}
-		else {
+
+		if (join_retval != 0) {
 			printf("pthread join error: %d\n", join_retval);
 		}
 	}
-
+	
+	printf("%0.14f\n", logValue);
+	printf("%0.14f\n", log(VALUE));
 	return 0;
 }
